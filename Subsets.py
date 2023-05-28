1864fbb21a65844b59ed1e7e1adf13b566d302a3
Nidhish Malviya <72844520+nidhish-create@users.noreply.github.com>
2023-05-28 20:04:34 +0530

    Create Subsets.py

---
+++ Subsets.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subsets(self, nums):
+        
+        ans =[[]]
+            
+        for i in nums:
+            for j in range(len(ans)):
+                ans.append(ans[j] + [i])
+            
+        return ans
+                
+            
+        
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
