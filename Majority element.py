393ee72306d1c79f842b3e4f58322cda08cf5e2b
Nidhish Malviya <72844520+nidhish-create@users.noreply.github.com>
2023-05-30 22:53:27 +0530

    Create Majority element.py

---
+++ Majority element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        dict = {}
+        for i in range(len(nums)):
+            
+            if nums[i] not in dict:
+                dict[nums[i]] = 1
+            else:
+                dict[nums[i]] += 1
+                
+        values = list(dict.values())
+        keys = list(dict.keys())
+        
+        k = 0
+        
+        for i in range(len(values)):
+            
+            if values[i] > (len(nums)/2):
+                k = keys[i]
+        return k
