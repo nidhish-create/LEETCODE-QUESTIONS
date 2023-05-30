9e3f67d300bd36617bae365babc616ec6421ddbc
Nidhish Malviya <72844520+nidhish-create@users.noreply.github.com>
2023-05-30 22:52:34 +0530

    Create  Shuffle an Array.py

---
+++ Shuffle an Array.py
@@ -0,0 +1,32 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        
+        self.nums = nums 
+        """
+        :type nums: List[int]
+        """
+        
+
+    def reset(self):
+        return self.nums
+        """
+        :rtype: List[int]
+        """
+        
+
+    def shuffle(self):
+        arr = self.nums[:]
+        n = len(arr)
+        for i in range(n):
+            rand = random.randint(i, n-1)
+            arr[i], arr[rand] = arr[rand], arr[i]
+        return arr 
+    
+        
+        
+        
+        
+        """
+        :rtype: List[int]
+        """
